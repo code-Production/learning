0a5ea041d6efd9454a0bc000203cafee0e763da0
code-Production <censore.it@gmail.com>
2022-04-29 17:19:42 +0300

    lesson 2

---
+++ src/main/java/ru/geekbrains/java_core_2/lesson_2/MyArrayDataException.java
@@ -0,0 +1,7 @@
+package ru.geekbrains.java_core_2.lesson_2;
+
+public class MyArrayDataException extends RuntimeException{
+    public MyArrayDataException(String message) {
+        super(message);
+    }
+}
